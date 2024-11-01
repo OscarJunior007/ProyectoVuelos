003a0e8a2a40ad977648e8ed26ee578e33790167
OscarJunior007 <159711525+OscarJunior007@users.noreply.github.com>
2024-10-31 22:37:57 -0500

    Agregar archivos de proyecto.

---
+++ Usuarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaVuelos
+{
+    public class Usuarios
+    {
+        public string Usuario { get; set; }
+        public string Contrasena { get; set; }
+
+        public  Usuarios(string usuario, string contrasena)
+        {
+            Usuario = usuario;
+            Contrasena = contrasena;
+        }
+
+    }
+}
---
+++ ValidarDatos.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace BibliotecaVuelos
+{
+    public class ValidarDatos
+    {
+        private List<Usuarios> users = new List<Usuarios>();
+
+       
+
+        public bool GuardarUsuario(string usuario, string contrasena)
+        {
+
+            bool  x = false;
+            try
+            {
+                
+                 if(File.Exists(@"C:\Users\Oscar\source\repos\VentanaLogin\bin\Debug\DATOS GUARDADOS\" + $"{usuario}.txt"))
+                {
+                    x = false;
+                }
+                else
+                {
+                    TextWriter GuardarDatos = new StreamWriter(@"C:\Users\Oscar\source\repos\VentanaLogin\bin\Debug\DATOS GUARDADOS\" + $"{usuario}.txt", true);
+                    GuardarDatos.WriteLine(contrasena);
+                    GuardarDatos.Close();
+                   x = true;
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return x;
+
+            //    if (users.Any(user => user.Usuario == usuario ))
+            //    {
+            //        return false;
+            //    }
+            //    else
+            //    {
+
+            //        users.Add(new Usuarios(usuario, contrasena));
+
+
+
+            //    }
+
+        }
+
+       
+        public bool RectificarDatos(string usuario, string contrasena)
+        {
+            bool encontrado = false;
+
+            try
+            {
+                using (TextReader VerificarDatos = new StreamReader(@"C:\Users\Oscar\source\repos\VentanaLogin\bin\Debug\DATOS GUARDADOS\" + $"{usuario}.txt" ))
+                {
+                    string ContraGuardada = VerificarDatos.ReadLine();
+
+                    
+                    if (ContraGuardada == contrasena)
+                    {
+                        encontrado = true;
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+
+                return false;
+            }
+            
+
+            return encontrado;
+
+
+      
+            
+            //return users.Any(user => user.Usuario == usuario && user.Contrasena == contrasena);
+
+        }
+    }
+
+    
+}
